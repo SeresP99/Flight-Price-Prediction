4a2ddb0fcd60300e9b735427f59c8b19a54277c3
SeresP99 <71933391+SeresP99@users.noreply.github.com>
2024-06-19 16:32:21 +0200

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,5 @@
+CONTINUOUS_COLUMNS = ['duration', 'price']
+
+DISCRETE_COLUMNS = ['days_left']
+
+NOMINAL_COLUMNS = ['airline', 'source_city', 'departure_time', 'stops', 'arrival_time', 'destination_city', 'class']
